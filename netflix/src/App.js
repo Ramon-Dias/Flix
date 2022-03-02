33ac57a7be76d513b546e0e47f5f23b8dea834d8
Ramon Dias Ferreira <ramondf13@gmail.com>
2022-03-02 20:45:28 -0300

    filme em destaque terminado

--- netflix/src/App.js
+++ netflix/src/App.js
@@ -1,31 +1,39 @@
-import React, {useEffect, useState} from "react";
-import "./App.css"
+import React, { useEffect, useState } from "react";
+import "./App.css";
 import MovieRow from "./components/MovieRow";
 import Tmdb from "./Tmdb";
+import FeatureMovie from "./components/FeatureMovie";
 
 const App = () => {
-
-  const [movieList, setMovieList] = useState([])
+  const [movieList, setMovieList] = useState([]);
+  const [feature, setFeature] = useState(null);
 
   useEffect(() => {
     const loadAll = async () => {
-      let list = await Tmdb.getHomeList()
-      setMovieList(list)
-    }
-    loadAll()
-  }, [])
+      let list = await Tmdb.getHomeList();
+      setMovieList(list);
 
-
+      let originals = list.filter(i=> i.slug === "originals")
+      let random = Math.floor(Math.random() * (originals[0].items.results.length -1))
+      let chosen = originals[0].items.results[random]
+      let chosenInfo = await Tmdb.getMovieInfo(chosen.id, "tv" )
+      setFeature(chosenInfo)
+ 
+    };
+    loadAll();
+  }, []);
 
   return (
     <div className="page">
+      {feature && <FeatureMovie item={feature} />}
+
       <section className="lists">
         {movieList.map((item, key) => (
-            <MovieRow key={key} title={item.title} items={item.items}/>
+          <MovieRow key={key} title={item.title} items={item.items} />
         ))}
       </section>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
